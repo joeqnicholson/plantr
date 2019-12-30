f1e80a06005eaae8fb1b8f01a72cf44a8a4e8a58
Ed Herman <edward.herman.87@gmail.com>
2019-12-30 11:12:06 -0500

    Update app.js with body parser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-const db = require("./config/key").mongoURI;
+const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const plants = require("./routes/api/plants");
 
 mongoose
@@ -9,6 +12,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+// Middleware for body parser:
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes:
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/plants", plants);
 
