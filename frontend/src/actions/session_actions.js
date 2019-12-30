ab0220274cc6083b377c5bd76000165b3f709201
kenny <kenny.lozeau@gmail.com>
2019-12-30 15:12:30 -0500

    Fix typo

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -3,7 +3,7 @@ import jwt_decode from 'jwt-decode';
 
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
-export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 // sets isAuthenticated to false when invoking logout thunk action
