d64303ea3f7b12978e8ab3cb7f21e990141b6054
kenny <kenny.lozeau@gmail.com>
2019-12-30 12:33:29 -0500

    Fix package dependencies and create SplashPage component

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as APIUtil from './util/session_api_util';
+import * as APIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddlware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -8,7 +8,7 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddlware(thunk, logger)
+    applyMiddleware(thunk, logger)
   )
 );
 
