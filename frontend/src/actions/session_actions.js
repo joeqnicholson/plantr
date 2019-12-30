d8595cb7fe57b269d299e928537e6e7c0442fcda
kenny <kenny.lozeau@gmail.com>
2019-12-30 11:27:47 -0500

    Create logout actions in session_actions

---
+++ frontend/src/actions/session_actions.js
@@ -0,0 +1,19 @@
+import * as APIUtil from './util/session_api_util';
+import jwt_decode from 'jwt-decode';
+
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+
+export const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT
+});
+
+export const logout = () => dispatch => {
+  // first remove the token from local storage
+  localStorage.removeItem('jwtToken');
+
+  // then remove the token from the common axios headers
+  APIUtil.setAuthToken(false);
+
+  // then dispatch the action to logout the user
+  dispatch(logoutUser());
+};
