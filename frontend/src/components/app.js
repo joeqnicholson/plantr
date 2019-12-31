398e2479697aaa0929c2c70c5b3d975e2e65cb7a
Joe Nicholson <joeqnicholson@gmail.com>
2019-12-31 09:02:38 -0500

    splash garbage

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
-import SplashPage from './splash_page';
+import SplashPage from './splash_page.jsx';
 
 const App = () => (
   <Switch>
