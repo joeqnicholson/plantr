a07154999043a6da4142fe3e4bd4b3e5f1e1a694
mauriceg123 <mg3183@columbia.edu>
2019-12-31 13:42:32 -0500

    set up garden component and container

---
+++ frontend/src/components/garden/garden.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../garden.css';
+
+class Garden extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="garden-outer-wrapper">
+                <div className="garden-inner-wrapper">
+                    <div className="garden-header">
+                        
+                    </div>
+                    <div className="garden-body">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Garden;
--- frontend/src/components/garden/garden_container.js
+++ frontend/src/components/garden/garden_container.js
@@ -1,1 +1,16 @@
 import {connect} from 'react-redux';
+import Garden from './garden';
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Garden);
