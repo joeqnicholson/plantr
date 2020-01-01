5f1141c584f00008d8fc9eabd62f9e95f3961c22
Ed Herman <edward.herman.87@gmail.com>
2020-01-01 12:09:23 -0500

    Add auth for ownedPlants CRUD, Add ownPlants update route

--- routes/api/ownedPlants.js
+++ routes/api/ownedPlants.js
@@ -10,46 +10,66 @@ const validateOwnedPlantInput = require('../../validation/ownedPlants');
 //   res.json({ msg: "This is the ownedPlants route" })
 // })
 
-router.get("/:userId", (req, res) => {
-  OwnedPlant.find({userId: req.params.userId})
-    .then(ownedPlants => {
-      res.json(ownedPlants);
-    })
-    .catch(err => {
-      res
-        .status(404)
-        .json({ errMsg: "There was an issue retreiving your plants" })
-    });
-      // ownedPlantsObj = {};
-      // debugger
-      // ownedPlants.forEach(ownedPlant => {
-      //   ownedPlantsObj[ownedPlant.id] = {
-      //     nickname: ownedPlant.nickname,
-      //     plantId: ownedPlant.plantId
-      //   }
-
-      // })
-    
-  
-})
-
-router.post("/:userId", (req, res) => {
-  const { errors, isValid } = validateOwnedPlantInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors)
+router.get("/:userId", passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    OwnedPlant.find({userId: req.params.userId})
+      .then(ownedPlants => {
+        res.json(ownedPlants);
+      })
+      .catch(err => {
+        res
+          .status(404)
+          .json({ errMsg: "There was an issue retreiving your plants" })
+      });
   }
+)
 
-  debugger
-  const nickname = req.body.nickname || ' ';
+router.post("/:userId", passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const nickname = req.body.nickname || ' ';
+    
+    let newOwnedPlant = {
+      userId: req.params.userId,
+      plantId: req.body.plantId,
+      nickname: nickname
+    }
 
-  const newOwnedPlant = new OwnedPlant({
-    userId: req.body.userId,
-    plantId: req.body.plantId,
-    nickname: nickname
-  })
+    // Validate ownedPlant data
+    const { errors, isValid } = validateOwnedPlantInput(newOwnedPlant);
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
 
-  newOwnedPlant.save().then(ownedPlant => res.json(ownedPlant));
-})
+    newOwnedPlant = new OwnedPlant(newOwnedPlant)
+
+    newOwnedPlant.save().then(ownedPlant => res.json(ownedPlant));
+  }
+)
+
+router.patch("/:userId/:ownedPlantId", 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+
+    const nickname = req.body.nickname || ' ';
+
+    let updatedOwnedPlant = {
+      userId: req.params.userId,
+      plantId: req.body.plantId,
+      nickname: nickname
+    }
+
+    // Validate ownedPlant data
+    const { errors, isValid } = validateOwnedPlantInput(updatedOwnedPlant);
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    const targetOwnedPlant = { _id: req.params.ownedPlantId }
+    OwnedPlant.updateOne(targetOwnedPlant, updatedOwnedPlant)
+      .then(() => {
+        res.json(updatedOwnedPlant)
+      });
+  }
+)
 
 module.exports = router;
--- validation/ownedPlants.js
+++ validation/ownedPlants.js
@@ -12,21 +12,9 @@ module.exports = function validateOwnedPlantsInput(data) {
     errors.userId = "Missing userId key"
   }
 
-  // if (!Validator.isInt(data.userId)) {
-  //   errors.userId = "Invalid userId - must be an integer"
-  // }
-
   if (Validator.isEmpty(data.plantId)) {
     errors.plantId = "Missing plantId key"
   }
-
-  // if (!Validator.isInt(data.plantId)) {
-  //   errors.plantId = "Invalid plantId - must be an integer"
-  // }
-
-  // if (Validator.isEmpty(data.nickname)) {
-  //   errors.nickname = ""
-  // }
 
   return {
     errors,
